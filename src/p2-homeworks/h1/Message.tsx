6391c19dcf8350d8f64e9fce9f004c3243d1c83a
olegweremyew <olegweremey1994@mail.ru>
2021-11-14 14:32:00 +0300

    Monday-Homework 1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import c from "./Message.module.css"
 
-function Message() {
+export type messageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+
+}
+
+function Message(props: messageDataType) {
     return (
-        <div>
-
+        <div className={c.message}>
+            <div className={c.avatar}>
+                <div className={c.ava}>
+                    <img src={props.avatar} title="ava"/>
+                </div>
+                <div className={c.name}>
+                    {props.name}
+                </div>
+            </div>
+            <div className={c.text}>
+                <div>
+                    {props.message}
+                </div>
+                <div className={c.time}>
+                    {props.time}
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Message
+export default Message;
