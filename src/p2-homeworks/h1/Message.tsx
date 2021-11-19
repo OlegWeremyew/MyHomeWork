9ea9b2676d501385512f6dd46a97e553c132c598
olegweremyew <olegweremey1994@mail.ru>
2021-11-19 21:42:40 +0300

    Monday-Homework 1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -11,24 +11,7 @@ export type messageDataType = {
 
 function Message(props: messageDataType) {
     return (
-        <div className={c.message}>
-            <div className={c.avatar}>
-                <div className={c.ava}>
-                    <img src={props.avatar} title="ava"/>
-                </div>
-                <div className={c.name}>
-                    {props.name}
-                </div>
-            </div>
-            <div className={c.text}>
-                <div>
-                    {props.message}
-                </div>
-                <div className={c.time}>
-                    {props.time}
-                </div>
-            </div>
-        </div>
+
     )
 }
 
