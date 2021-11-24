a0d92b71cfbb3aa2d93e71cccdea2ea926b37d4c
olegweremyew <olegweremey1994@mail.ru>
2021-11-24 22:28:10 +0300

    second task (1 homework)

--- src/p2-homeworks/h1/AlternativeMessage.tsx
+++ src/p2-homeworks/h1/AlternativeMessage.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import c from "./AlternativeMessage.module.css"
 
-function AlternativeMessage() {
+export type AlternativeMessageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+
+}
+
+function AlternativeMessage(props: AlternativeMessageDataType) {
     return (
-        <div>
+        <div className={c.message}>
+            <img src={props.avatar} alt="avatar"/>
+
+            <div className={c.radius}/>
+
+            <div className={c.content}>
+                <div className={c.name}>{props.name}</div>
+                <div className={c.text}>{props.message}</div>
+                <div className={c.time}> {props.time}</div>
+            </div>
 
         </div>
     )
 }
 
-export default AlternativeMessage
+export default AlternativeMessage;
+
