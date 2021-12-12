b4182a567d3861d5a2c3b997c19590cd0933e4f4
olegweremyew <olegweremey1994@mail.ru>
2021-12-12 20:46:07 +0300

    buttons

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -38,21 +38,22 @@ function HW4() {
                 />
 
                 {/*----------------------------------------------------*/}
+                <div className={s.buttons}>
+                    <SuperButton>
+                        default
+                    </SuperButton>
 
-                <SuperButton>
-                    default
-                </SuperButton>
+                    <SuperButton
+                        red // пропсу с булевым значением не обязательно указывать true
+                        onClick={showAlert}
+                    >
+                        delete {/*// название кнопки попадёт в children*/}
+                    </SuperButton>
 
-                <SuperButton
-                    red // пропсу с булевым значением не обязательно указывать true
-                    onClick={showAlert}
-                >
-                    delete {/*// название кнопки попадёт в children*/}
-                </SuperButton>
-
-                <SuperButton disabled>
-                    disabled
-                </SuperButton>
+                    <SuperButton disabled>
+                        disabled
+                    </SuperButton>
+                </div>
 
                 {/*----------------------------------------------------*/}
 
